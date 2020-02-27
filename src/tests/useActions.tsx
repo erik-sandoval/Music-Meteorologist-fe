931f97d6155f8ec935675702824ef1f907f5f584
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-02-27 18:50:58 -0500

    updating testing suits

--- src/tests/useActions.tsx
+++ src/tests/useActions.tsx
@@ -1,13 +1,13 @@
 import postDSSong from '../Redux/DS/ds.actions';
 
-export const useActions = () => {
-    const ds: postDSSong = postDSSong();
-    return {
-        ds
-    };
-};
+// export const useActions = () => {
+//     const ds: postDSSong = postDSSong();
+//     return {
+//         ds
+//     };
+// };
 
-interface UseActions {
-    ds: postDSSong
-}
-export type Actions = UseActions | null;
+// interface UseActions {
+//     ds: postDSSong
+// }
+// export type Actions = UseActions | null;
