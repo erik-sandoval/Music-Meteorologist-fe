c0fbb11f42ae7eef2a2a279f1bcf056aeacb1247
Erik Sandoval <erik.sandoval@icloud.com>
2020-02-26 17:14:48 -0800

    Refactored some methods

--- src/components/dashboard/Song.js
+++ src/components/dashboard/Song.js
@@ -1,103 +1,116 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Grid, Typography } from '@material-ui/core';
-import { getTrackInfo, getSeveralTracks } from '../../Redux/Spotify/spotify.actions';
-import '../../App.css';
-import axios from 'axios';
+import React from "react";
+import { connect } from "react-redux";
+import { Grid, Typography } from "@material-ui/core";
+import { getTrackInfo } from "../../Redux/Spotify/spotify.actions";
+import "../../App.css";
+import axios from "axios";
 
 class Song extends React.Component {
-  constructor(props) { 
-    super(props);
-  }
-
   msToTime(s) {
-    var ms = s % 1000;
-    s = (s - ms) / 1000;
-    var secs = s % 60;
-    s = (s - secs) / 60;
-    var mins = s % 60;
-    var hrs = (s - mins) / 60;
-    if (secs === 0) {
-      return mins + ':' + '00'
-    } else if (secs < 10) {
-      return mins + ':' + '0' + secs;
-    } else { 
-      return mins + ':' + secs;
-    }
+    var minutes = Math.floor(s / 60000);
+    var seconds = ((s % 60000) / 1000).toFixed(0);
+    return seconds == 60
+      ? minutes + 1 + ":00"
+      : minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
   }
 
+  changeSong = trackUris => {
+    axios.put(
+      `https://api.spotify.com/v1/me/player/play?device_id=${this.props.deviceId}`,
+      {
+        uris: trackUris
+      },
+      {
+        headers: { Authorization: "Bearer " + localStorage.getItem("token") }
+      }
+    );
+  };
 
   render() {
+    const { song } = this.props;
 
-    const trackUris = this.props.tracks.map(track => track.uri)
-    trackUris.unshift(this.props.song.uri)
-    const changeSong = () => {
-      console.log("changeSong", this.props)
-      console.log("uri array", trackUris)
-      axios.put(
-        `https://api.spotify.com/v1/me/player/play?device_id=${this.props.deviceId}`,
-        {
-          "uris": trackUris,
-        },
-        { headers: { Authorization: 'Bearer ' + localStorage.getItem('token') } }
-      )
-    }
+    const trackUris = this.props.tracks.map(track => track.uri);
+    trackUris.unshift(song.uri);
+
+    console.log("Song Component props", this.props);
 
     return (
       <div>
-        <Grid style={{borderRadius: "5px"}}
+        <Grid
+          style={{ borderRadius: "5px" }}
           container
-          direction='row'
-          alignItems='center'
-          wrap='wrap'
-          className='song'>
-          <Grid item>
-          <button className="playflex"
-            style={{
-            background: "none",
-            border: "none",
-            outline: "none",
-            }}
+          direction="row"
+          alignItems="center"
+          wrap="wrap"
+          className="song"
         >
-            {"playing" ? (
-                <a onClick={changeSong} className="playicon2" />
-            ) : (
-            <a className="playicon" style={{ maxHeight: 35 }} />
-            )}
+          <Grid item>
+            <button
+              className="playflex"
+              style={{
+                background: "none",
+                border: "none",
+                outline: "none"
+              }}
+            >
+              {"playing" ? (
+                <a
+                  onClick={() => this.changeSong(trackUris)}
+                  className="playicon2"
+                />
+              ) : (
+                <a className="playicon" style={{ maxHeight: 35 }} />
+              )}
             </button>
-            </Grid>
+          </Grid>
           <Grid item style={{ padding: 5 }}>
-            <img style={{ borderRadius: '5px', marginRight: '20px'}}
-              src={this.props.song.album.images[2].url}
-              alt='album art'
-              width='64px'
+            <img
+              style={{ borderRadius: "5px", marginRight: "20px" }}
+              src={song.album.images[2].url}
+              alt="album art"
+              width="64px"
             />
           </Grid>
-          <Grid item style={{ padding: 5, width: '200px'}}>
+          <Grid item style={{ padding: 5, width: "200px" }}>
             <Typography
               className="songName"
-              style={{ fontSize: 13, fontWeight: 'bold', marginBottom: '3px' }}
-              direction='row'>
-              {this.props.song.name}
+              style={{ fontSize: 13, fontWeight: "bold", marginBottom: "3px" }}
+              direction="row"
+            >
+              {song.name}
             </Typography>
             <Typography className="songArtistName" style={{ fontSize: 13 }}>
-              {this.props.song.artists[0].name}
+              {song.artists[0].name}
             </Typography>
-            <Typography style={{ fontSize: 13 }}>
-              {console.log("inside songjs", this.props)}
-            </Typography>
+            <Typography style={{ fontSize: 13 }}></Typography>
             {/* <p>Audio Features: {loadingTf ? 'loading....' : tf.data.tempo}</p> */}
           </Grid>
-          <Grid className="songAlbumName" item style={{ padding: 5, fontSize: 13, width: '200px', marginRight: '10px'}}>
-            {this.props.song.album.name}
+          <Grid
+            className="songAlbumName"
+            item
+            style={{
+              padding: 5,
+              fontSize: 13,
+              width: "200px",
+              marginRight: "10px"
+            }}
+          >
+            {song.album.name}
           </Grid>
-          <Grid className="songDuration" item style={{ padding: 5, fontSize: 13, width: '50px', marginLeft: 25 }}>
-            {this.msToTime(this.props.song.duration_ms)}
+          <Grid
+            className="songDuration"
+            item
+            style={{ padding: 5, fontSize: 13, width: "50px", marginLeft: 25 }}
+          >
+            {this.msToTime(song.duration_ms)}
           </Grid>
-          <Grid className="songReleaseDate" item style={{ padding: 5, fontSize: 13, width: '100px', marginLeft: 65 }}>
-            {this.props.song.album.release_date}
+          <Grid
+            className="songReleaseDate"
+            item
+            style={{ padding: 5, fontSize: 13, width: "100px", marginLeft: 65 }}
+          >
+            {song.album.release_date}
           </Grid>
-
         </Grid>
       </div>
     );
@@ -105,11 +118,7 @@ class Song extends React.Component {
 }
 
 const mapStateToProps = state => ({
-  tracksInfo: state.getTrackInfoReducer,
-  several_tracks: state.queueReducer.several_tracks,
+  tracksInfo: state.getTrackInfoReducer
 });
 
-export default connect(
-  mapStateToProps,
-  { getTrackInfo, getSeveralTracks },
-)(Song);
+export default connect(mapStateToProps, { getTrackInfo })(Song);
