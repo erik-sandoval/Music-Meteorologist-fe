fde7326881105472233081f1372aba2607030c89
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-02-27 15:46:05 -0500

    trying to add context for testing redux in the ts file q reducer path is not working

---
+++ src/tests/rooterReducer.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import { queueReducer } from '../Redux/reducers/queueReducer';
+
+export const rootReducer = combineReducers({
+    queueReducer
+});
+
+export type AppState = ReturnType<typeof rootReducer>
