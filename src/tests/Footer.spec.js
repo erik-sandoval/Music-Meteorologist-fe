343b8fba98bbda2601331d1b016111ac23bd6d8b
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-02-27 16:42:30 -0500

    added a testing suit for footer

---
+++ src/tests/Footer.spec.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from "react-redux";
+import configureStore from 'redux-mock-store';
+import { BrowserRouter as Router } from "react-router-dom";
+import Footer from '../components/Footer';
+
+const mockStore = configureStore([]);
+
+describe('Footer', () => {
+    let store;
+    beforeEach(() => {
+        store = mockStore({
+            admin: {
+                isAdmin: false
+            }
+        })
+    })
+    it('should render without crashing', () => {
+        const wrapper = render(<Provider store={store}><Router><Footer /></Router></Provider>);
+        const copyRight = wrapper.getByText(/©/i)
+        expect(copyRight).toBeInTheDocument
+    })
+});
