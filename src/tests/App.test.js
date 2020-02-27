2fe7186cd0725c212676b3504351b063bc312e20
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-02-27 17:54:08 -0500

    added cory's branch for testing suits auth, app, dash,logout

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -4,11 +4,6 @@ import App from '../App';
 import { BrowserRouter } from 'react-router-dom';
 import * as rtl from '@testing-library/react';
 
-// it('renders without crashing', () => {
-//   const div = document.getElementById('root');
-//   ReactDOM.render(<App />, div);
-// });
-
 test('renders without crashing', () => {
   rtl.render(<BrowserRouter><App /></BrowserRouter>)
 });
