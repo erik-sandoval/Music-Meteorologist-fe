f549c0baf2ba149bab2bb0aa71c22fa4688fd763
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-04 20:09:43 -0800

    Removed Darkmode feature

--- src/components/dashboard/element-styles/Dropdown.js
+++ src/components/dashboard/element-styles/Dropdown.js
@@ -19,7 +19,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const MenuListComposition = props => {
+const DropDownMenu = props => {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
@@ -59,45 +59,6 @@ const MenuListComposition = props => {
     prevOpen.current = open;
   }, [open]);
 
-  function toggleLightDark() {
-    var element = document.getElementById("sideBarLD");
-    element.classList.toggle("sideBarL");
-    var element1 = document.getElementById("sideBarLD1");
-    element1.classList.toggle("sideBarL");
-    var element2 = document.getElementById("sideBarLD2");
-    element2.classList.toggle("sideBarL");
-    var element3 = document.getElementById("radar1");
-    element3.classList.toggle("radarL");
-    var element4 = document.getElementById("playInfoLD");
-    element4.classList.toggle("playInfoL");
-    var element5 = document.getElementById("mainBarLD");
-    element5.classList.toggle("mainBarL");
-    var element6 = document.getElementById("mainBarLD1");
-    element6.classList.toggle("mainBarL");
-    var element7 = document.getElementById("songLD");
-    element7.classList.toggle("mainBarL");
-    var element8 = document.querySelector("#playpause");
-    element8.classList.toggle("playpauseL");
-    var element9 = document.querySelector("#prev");
-    element9.classList.toggle("nextPrevL");
-    var element10 = document.querySelector("#next");
-    element10.classList.toggle("nextPrevL");
-    var element11 = document.querySelector("#x");
-    element11.classList.toggle("x");
-    var element12 = document.querySelector("#heart");
-    element12.classList.toggle("heart");
-    var element13 = document.querySelector(".MuiLinearProgress-colorPrimary");
-    element13.classList.toggle("progressBarEmpty");
-    var element14 = document.querySelector(
-      ".MuiLinearProgress-barColorPrimary"
-    );
-    element14.classList.toggle("progressBarFull");
-    var element15 = document.querySelectorAll(".playicon2");
-    element15.forEach(element => element.classList.toggle("playiconL"));
-    var element16 = document.querySelector(".playlisticon");
-    element16.classList.toggle("playlistblack");
-  }
-
   return (
     <div className={classes.root}>
       <div>
@@ -135,13 +96,6 @@ const MenuListComposition = props => {
                     id="menu-list-grow"
                     onKeyDown={handleListKeyDown}
                   >
-                    <MenuItem className="copybar" onClick={toggleLightDark}>
-                      Dark/Light Mode
-                    </MenuItem>
-                    {/* <MenuItem className="copybar">
-                      Your Spotify ID <br />
-                      <input className="copytext" type="text" value={props.navBarProps.spotifyId.id} id="myInput" />
-                    </MenuItem> */}
                     <MenuItem className="copybar" onClick={logout}>
                       Logout
                     </MenuItem>
@@ -156,4 +110,4 @@ const MenuListComposition = props => {
   );
 };
 
-export default withRouter(MenuListComposition);
+export default withRouter(DropDownMenu);
