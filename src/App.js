4f917962b26186db40c60c3e29f0ef847c5c501c
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-06 15:59:46 -0800

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
-import { ProtectedRoute } from "./components/protected.route";
 
-import Auth from "./components/Auth";
-import MusicPlayer from "./components/dashboard/MusicPlayer";
-
-import Logout from "./views/Logout";
-import Dashboard from "./views/Dashboard.js";
-import "./App.css";
 import About from "./pages/about/about.component";
-import Team from "./components/Team";
+import Team from "./pages/team/team.component";
+
+import "./App.css";
+import Landing from "./pages/landing/landing.component";
 
 /* import LikedSongs from './components/LikedSongs'; */
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <Route exact path='/' render={props => <Auth {...props} />} />
-        <Route exact path='/logout' component={Logout} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/team' component={Team} />
-        <ProtectedRoute exact path='/dashboard' component={Dashboard} />
+      <div className="App">
+        <Route exact path="/" render={props => <Landing {...props} />} />
+        {/* <Route exact path='/logout' component={Logout} /> */}
+        <Route exact path="/about" component={About} />
+        <Route exact path="/team" component={Team} />
+        {/* <ProtectedRoute exact path='/dashboard' component={Dashboard} /> */}
       </div>
     );
   }
