d9c8ccad1d81f7353f679784182bd82d8df95d14
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-06 15:54:05 -0800

    restarting app from the ground up, refactoring into new file style.

--- src/components/dashboard/element-styles/MainBarContainer.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export default styled.div`
-  height: 90vh;
-  width: 100%;
-  background: #17191c;
-  font-family: Work Sans;
-`;
--- src/components/dashboard/element-styles/PlaylistSongs.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export default styled.div`
-  height: auto;
-  width: 100%;
-  background: #17191c;
-  font-family: Work Sans;
-`;
