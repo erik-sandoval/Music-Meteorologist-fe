e4b0a2ab46567a3eb5887717aa1259d1e4a735e4
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-06 16:30:50 -0800

    Created Loading component

---
+++ src/pages/loading/loading.styles.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const LoadContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  width: 100%;
+  height: 100%;
+  margin-top: 50vh;
+`;
