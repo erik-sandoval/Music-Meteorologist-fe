4934dfa3edca574b8274cecc1c2e29dc6c2b0704
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-06 16:00:25 -0800

    auth button styles

---
+++ src/components/buttons/auth-button.styles.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export default styled.div`
+  padding-top: 15px;
+  padding-bottom: 15px;
+  margin-top: 35px;
+  font-weight: 500;
+  font-size: 20px;
+  color: white;
+  background-color: #e20351;
+  border: none;
+  border-radius: 5px;
+  width: 313px;
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+
+  &:hover {
+    background: #eb4c83;
+  }
+
+  @media (max-width: 500px) {
+    margin: 0%;
+    position: ${prop => (prop.noAbsolute ? "static" : "absolute")};
+    z-index: 1;
+    transform: translate(0px, -125px);
+    transform: ${prop =>
+      prop.noTransform ? "translate(0px, 0px)" : "translate(0px, -125px)"};
+    font-size: 14px;
+    width: 190px;
+    height: 15px;
+    font-weight: 500;
+  }
+`;
