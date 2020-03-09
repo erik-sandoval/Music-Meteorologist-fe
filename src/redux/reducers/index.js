4e423349a342bdb49285946bef2eba553bd66d10
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-09 12:40:30 -0700

    Removing unused spotify actions

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,22 +3,23 @@ import getUserReducer from "./userReducer";
 import likedSongsReducer from "./likedSongsReducer";
 import getTrackInfoReducer from "./getTrackInfoReducer";
 import currentSongReducer from "./getCurrentSongReducer";
-import queueReducer from "./queueReducer";
 import createPlaylistReducer from "./createPlaylistReducer";
 import getPlaylistReducer from "./getPlaylistReducer";
 import getCurrentUserReducer from "./getCurrentUserReducer";
 import addToPlaylistReducer from "./addToPlaylistReducer";
 import removeTrackReducer from "./removeTrackReducer";
+import getSongsReducer from "./getSongsReducer";
 
 export default combineReducers({
-  getUserReducer,
-  likedSongsReducer,
-  getTrackInfoReducer,
-  currentSongReducer,
-  queueReducer,
-  createPlaylistReducer,
-  getPlaylistReducer,
-  getCurrentUserReducer,
-  addToPlaylistReducer,
-  removeTrackReducer
+  getSongsReducer
+  // getUserReducer,
+  // likedSongsReducer,
+  // getTrackInfoReducer,
+  // currentSongReducer,
+  // queueReducer,
+  // createPlaylistReducer,
+  // getPlaylistReducer,
+  // getCurrentUserReducer,
+  // addToPlaylistReducer,
+  // removeTrackReducer
 });
