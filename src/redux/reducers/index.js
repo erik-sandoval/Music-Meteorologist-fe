9e05ed466b384a236bbaea7e1f901d0868c6bddd
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-09 16:24:07 -0700

    seperated spotify and ds song into two different reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,25 +1,8 @@
 import { combineReducers } from "redux";
-import getUserReducer from "./userReducer";
-import likedSongsReducer from "./likedSongsReducer";
-import getTrackInfoReducer from "./getTrackInfoReducer";
-import currentSongReducer from "./getCurrentSongReducer";
-import createPlaylistReducer from "./createPlaylistReducer";
-import getPlaylistReducer from "./getPlaylistReducer";
-import getCurrentUserReducer from "./getCurrentUserReducer";
-import addToPlaylistReducer from "./addToPlaylistReducer";
-import removeTrackReducer from "./removeTrackReducer";
-import getSongsReducer from "./getSongsReducer";
+import getDSSongsReducer from "./getDSSongData";
+import getSpotifySongsReducer from "./getSpotifySongsReducer";
 
 export default combineReducers({
-  getSongsReducer
-  // getUserReducer,
-  // likedSongsReducer,
-  // getTrackInfoReducer,
-  // currentSongReducer,
-  // queueReducer,
-  // createPlaylistReducer,
-  // getPlaylistReducer,
-  // getCurrentUserReducer,
-  // addToPlaylistReducer,
-  // removeTrackReducer
+  getDSSongsReducer,
+  getSpotifySongsReducer
 });
