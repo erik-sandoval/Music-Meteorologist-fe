6531adc9d0683b3ebaadad7fa9741bd99f89819e
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-09 19:08:00 -0700

    Had to initialize sdk in order to get a device id to transfer playback.

--- src/pages/dashboard/dashboard.component.jsx
+++ src/pages/dashboard/dashboard.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import LoadingPage from "../loading/loading.component";
@@ -6,21 +6,63 @@ import LoadingPage from "../loading/loading.component";
 import MusicPlayer from "../../components/music-player/music.player.component";
 
 import { postDSSong } from "../../redux/ds/ds.actions";
-
+import {
+  getSeveralTracks,
+  getSpotifyUser,
+  getCurrentSong
+} from "../../redux/spotify/spotify.actions";
 // Styling
 import "../../App.css";
+import { transferPlaybackHere } from "../../utils/playerActions";
 
-class Dashboard extends React.Component {
+class Dashboard extends Component {
   state = {
     collapse: false,
     popout: false,
-    playlistCreated: false,
-    userDataFetching: false
+    playlistCreated: false
   };
 
   componentDidMount() {
-    this.props.postDSSong(localStorage.getItem("token"));
+    const { postDSSong, getSpotifyUser, getCurrentSong } = this.props;
+    const token = localStorage.getItem("token");
+
+    getSpotifyUser();
+    postDSSong(token);
   }
+
+  componentDidUpdate() {
+    const { dsSongsData, getSeveralTracks, getCurrentSong } = this.props;
+
+    this.initializePlayer();
+    getCurrentSong();
+    this.player.setVolume(0.5);
+    getSeveralTracks(dsSongsData);
+  }
+
+  // this is needed in order to get a device id to transfer playback to browser
+  initializePlayer = () => {
+    const { getCurrentSong } = this.props;
+    const token = localStorage.getItem("token");
+
+    this.player = new window.Spotify.Player({
+      name: "Sound Drip Spotify Player",
+      getOAuthToken: cb => {
+        cb(token);
+      }
+    });
+
+    this.player.on("player_state_changed", spotifyState => {
+      getCurrentSong(spotifyState);
+    });
+
+    this.player.on("ready", data => {
+      const { device_id } = data;
+
+      transferPlaybackHere(token, device_id);
+    });
+
+    this.player.connect();
+  };
 
   logout = e => {
     e.preventDefault();
@@ -33,7 +75,7 @@ class Dashboard extends React.Component {
       <div className="dashboard">
         {this.props.fetchingDsSongs ? (
           <div>
-            <MusicPlayer spotifyId={this.props.spotifyUser} />
+            <MusicPlayer />
           </div>
         ) : (
           <LoadingPage />
@@ -44,7 +86,12 @@ class Dashboard extends React.Component {
 }
 
 const mapStateToProps = state => ({
-  dsSongsData: state.getSongsReducer.dsSongsData
+  dsSongsData: state.dsSongs.dsSongsData
 });
 
-export default connect(mapStateToProps, { postDSSong })(Dashboard);
+export default connect(mapStateToProps, {
+  postDSSong,
+  getSeveralTracks,
+  getSpotifyUser,
+  getCurrentSong
+})(Dashboard);
