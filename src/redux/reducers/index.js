379642fef40a890df84e2eecdae209fe18c7c75f
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-10 21:09:59 -0700

    renamed reducer files with a .reducer.js to help organize file structure more.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,13 @@
 import { combineReducers } from "redux";
-import getDSSongsReducer from "./getDSSongData";
-import getSpotifySongsReducer from "./getSpotifySongsReducer";
-import getCurrentUserReducer from "./getCurrentUserReducer";
-import getCurrentSongReducer from "./getCurrentSongReducer";
-
+import getDSSongsReducer from "./getDSSongData.reducer";
+import getSpotifySongsReducer from "./getSpotifySongs.reducer";
+import getCurrentUserReducer from "./getCurrentUser.reducer";
+import getCurrentSongReducer from "./getCurrentSong.reducer";
+import getChartTrackInfoReducer from "./getChartTrackInfo.reducer";
 export default combineReducers({
   dsSongs: getDSSongsReducer,
   spotifyUris: getSpotifySongsReducer,
   currentUser: getCurrentUserReducer,
-  currentSong: getCurrentSongReducer
+  currentSong: getCurrentSongReducer,
+  chartInfo: getChartTrackInfoReducer
 });
