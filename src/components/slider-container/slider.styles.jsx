b02ee55d7a5e6a21075fd3492e42115b9bd099b0
Erik Sandoval <erik.sandoval@icloud.com>
2020-03-12 18:49:15 -0700

    slider styles finished

--- src/components/slider-container/slider.styles.jsx
+++ src/components/slider-container/slider.styles.jsx
@@ -1,41 +1,54 @@
 import styled from "styled-components";
 
-export const SliderContainerDiv = styled.div`
-  position: absolute;
+export const OuterSliderContainerDiv = styled.div`
+  height: 450px;
+  background: #1c1e22;
+  display: flex;
+  flex-direction: column;
+  width: 1063px;
+  align-items: center;
+`;
+
+export const InnerSliderContainerDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
   width: 1063px;
   height: 324px;
-  left: 377px;
-  top: 84px;
+  background: #1c1e22;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const SliderValueContainer = styled.div`
+  margin-right: 5%;
   width: 186px;
   height: 68px;
   left: 1152px;
   top: 219px;
+
+  &:nth-of-type(4n + 4) {
+    margin-right: 0;
+  }
 `;
 
 export const SliderCategoryName = styled.div`
   width: 39px;
   height: 16px;
-  left: 482px;
-  top: 128px;
-
   font-family: Work Sans;
   font-style: normal;
   font-weight: 600;
   font-size: 14px;
   line-height: 16px;
-
   color: #ffffff;
 `;
 
 export const RangeSliderInput = styled.input`
   appearance: none;
   width: 179px;
-  height: 12px;
+  height: 8px;
   background: #a7d6f2;
   border-radius: 10px;
+  margin: 18% 0 5% 0;
 
   &::-webkit-slider-thumb {
     appearance: none;
@@ -48,17 +61,29 @@ export const RangeSliderInput = styled.input`
 `;
 
 export const RangeLevelNameDiv = styled.div`
-  width: 26px;
   height: 14px;
-  left: 480px;
-  top: 182px;
-
   font-family: Work Sans;
   font-style: normal;
   font-weight: normal;
   font-size: 12px;
   line-height: 14px;
-  text-align: ${props=> props.textAlign};
+  text-align: right;
   color: #ffffff;
   opacity: 0.7;
+`;
+
+export const GetNewSongsButton = styled.button`
+  margin-top: 2%;
+  width: 180px;
+  height: 55px;
+  background: #e20351;
+  border-radius: 6px;
+  font-family: Work Sans;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 18px;
+  text-align: center;
+  color: #ffffff;
+  border: none;
 `;
